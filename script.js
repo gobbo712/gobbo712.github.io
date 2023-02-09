4e35c095649d8f04f52426bdd23ec05be77a76f4
gobbo712 <gobbo712@users.noreply.github.com>
2023-02-09 20:41:14 -0300

    s

---
+++ script.js
@@ -0,0 +1,13 @@
+let webSocket = new WebSocket('ws://vps-3102527-x.dattaweb.com:9000');
+
+// Send text to all users through the server
+function sendHi(uuid) {
+    // Construct a msg object containing the data the server needs to process the message from the chat client.
+    const msg = {"type": "hi", "uuid": uuid};
+  
+    // Send the msg object as a JSON-formatted string.
+//    webSocket.send(JSON.stringify(msg));
+  
+    // Blank the text input element, ready to receive the next line of text from the user.
+    document.getElementById("dadiv").innerHTML = uuid;
+  }
